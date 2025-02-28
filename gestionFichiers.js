18e395478361e710fe1e2172635ec06c6399d891
Luce <boama561@gmail.com>
2025-02-28 12:48:12 +0000

    test

--- gestionFichiers.js
+++ gestionFichiers.js
@@ -1,21 +1,52 @@
 const fs = require('fs')
 
+// Creation de fichier avec Js --------------------------------------------
+// if (fs.existsSync('./monFichier')) {
+//     fs.rmdir('./monFichier', (erreur) => {
+//         if (erreur) {
+//             console.log(erreur)
+//         } else {
+//             console.log('votre dossier a ete supprimé')
+//         }
+//     })
+// } else {
 
-if (fs.existsSync('./monFichier')) {
-    fs.rmdir('./monFichier', (erreur) => {
-        if (erreur) {
-            console.log(erreur)
-        } else {
-            console.log('votre dossier a ete supprimé')
+//     fs.mkdir('./monFichier', (erreur) => {
+//         if (erreur) {
+//             console.log(erreur);
+//         } else {
+//             console.log('dossier créé avec succes')
+//         }
+//     });
+// }
+
+// creation de fichiers avec NodeJs ----------------------
+
+fs.writeFile('./monFichier/fichier1.txt', 'Bonjour ! je developpe en Node.Js', () =>{
+    console.log('fichier crée avec succés !')
+});
+
+// Lecture de fichiers avec NodeJs ----------------------
+
+fs.readFile('./monFichier/fichier1.txt', (erreur, donnee)=> {
+    if(erreur){
+        console.log(erreur);
+    }else{
+        console.log(donnee.toString());
+    }
+})
+
+// suppression de fichier avec node js ----------------------
+
+if(fs.existsSync('./monFichier/fichier1.txt')){
+    fs.unlink('./monFichier/fichier1.txt', (erreur)=> {
+        if (erreur){
+            console.log(erreur);
+            
+        }else{
+            console.log('suppression reussi')
         }
     })
-} else {
-
-    fs.mkdir('./monFichier', (erreur) => {
-        if (erreur) {
-            console.log(erreur);
-        } else {
-            console.log('dossier créé avec succes')
-        }
-    });
+}else{
+    console.log('impossible de supprimer un fichier inexistant')
 }
--- monServeur.js
+++ monServeur.js
@@ -1,22 +1,29 @@
 const http = require('http');
+const fs = require('fs')
 
 const serveur = http.createServer((requete, reponse) => {
     // Definition de l'entete 
     reponse.setHeader("content-type", "text/html");
-    
+
     // Definition de la reponse selon la demande du client
-    reponse.write("<head><meta charset='utf8'><head>")
+    let fichier ='';
     if (requete.url === "/acceuil"){
-        reponse.write("<h1>Acceuil</h1><p>Bienvenue sur l'acceuil<p>");
+        fichier = './HTML/acceuil.html'
     } 
     else if (requete.url === "/profil"){
-        reponse.write("<h1>Profil</h1><p>Bienvenue sur le profil<p>");
+        fichier = './HTML/profil.html'
     }else {
-        reponse.write("<h1>404 Page insdisponnible</h1><p>url incorrect<p>");
+        fichier = './HTML/erreur.html'
     }
 
-    // Finalisation de la reponse à envoyer
-    reponse.end();
+    fs.readFile(fichier, (erreur, donnée) =>{
+        if (erreur){
+            console.log(erreur)
+            reponse.end()
+        }else{
+            reponse.end(donnée)
+        }
+    })
 });
 
 serveur.listen(3001, "localhost", () => {
