f54fa707d71088e5bc4157af32042e7a3de597a2
Luce <boama561@gmail.com>
2025-02-28 01:47:51 +0000

    Premier Exercice Node.js: monServeur.js

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # nodeJs-execices
+
+<!-------------------------------- monServeur.js ------------------------->
+*monServeur.js:* est un serveur web basique en Node.js, capable de répondre à différentes requêtes selon l'URL demandée.
+✅ Répond à l'URL /acceuil avec un message de bienvenue.
+✅ Répond à l'URL /profil avec une page de profil.
+✅ Affiche une page 404 si l'URL est inconnue.
---
+++ monServeur.js
@@ -0,0 +1,25 @@
+const http = require('http');
+
+const serveur = http.createServer((requete, reponse) => {
+    // Definition de l'entete 
+    reponse.setHeader("content-type", "text/html");
+
+    // Definition de la reponse selon la demande du client
+    reponse.write("<head><meta charset='utf8'><head>")
+    if (require.url ==="/acceuil"){
+        reponse.write("<h1>Acceuil</h1><p>Bienvenue sur l'acceuil<p>");
+    } 
+    else if (requete.url ==="/profil"){
+        reponse.write("<h1>Profil</h1><p>Bienvenue sur le profil<p>");
+    }else {
+        reponse.write("<h1>404 Page insdisponnible</h1><p>url incorrect<p>");
+    }
+
+    // Finalisation de la reponse à envoyer
+    reponse.end();
+});
+
+serveur.listen(3000, "localhost", () => {
+    console.log("pret à écouter les requêtes");
+    
+});
